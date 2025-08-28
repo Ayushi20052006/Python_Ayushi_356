6a986c6559f4623963ebf33e663e482ecbdd89b5
Ayushi20052006 <ayushi20052006@gmail.com>
2025-08-28 14:27:46 +0530

    Covered basic overview of variables, datatypes, id of variables

--- Basic.py
+++ Basic.py
@@ -1,6 +1,37 @@
-def sum(a,b):
-    return(a+b)
+# i=1;
+# print(type(i));
+# print(id(i));
 
-a=int(input("Enter 1st number"))
-b=int(input("Enter 2nd number"))
-print(f"Sum of {a} and {b} is {sum(a,b)}")
+#multiple variable
+# a,b,c=1,2,3;
+# print(a,b,c);
+
+# print(id(a));
+# print(id(b));
+# print(id(c));
+
+# a=b=c;
+# print(id(a));
+# print(id(b));
+# print(id(c)); #(same as c)
+
+
+
+#variable naming-
+# 1-camel style:- myVariable
+# 2-snake style:- my_variable
+# 3-pascal style:- MyVariable
+
+#datatypes
+# var1=1  #integer
+# var2=True   #boolean
+# var3=10.567   #float
+# var4=10+6j     #complex
+# print(id(var1))
+# print(type(var1))
+# print(id(var2))
+# print(type(var2))
+# print(id(var3))
+# print(type(var3))
+# print(id(var4))
+# print(type(var4))
