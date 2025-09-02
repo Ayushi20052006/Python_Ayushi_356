a7488fcd82568cc0dd0366cb299734fe7313f7e6
Ayushi20052006 <ayushi.jha20052006@gmail.com>
2025-09-02 11:57:19 +0530

    Strng, List, Tupple

--- Basic.py
+++ Basic.py
@@ -60,3 +60,102 @@
 # a="Hello my name is Ayushi"
 # print(a[2:15])
 # print(a[0:5])
+
+# a=" Ayushi "
+# print(a.strip()) #removes spaces
+
+# #replace
+# print(a.replace("Hello","Ayushi"))
+
+# a="Hello, world"
+# print(a.split(","))  #['Hello', ' world']
+# a="Hello world, Ayushi here"
+# print(a.split(","))  #['Hello world', ' Ayushi here']
+#here "," works as a seperator
+
+#string concatenation
+# a="hello"
+# b="world"
+# c=a+" "+b
+# print(c)   #hello world
+
+# #format-strings (F-string)
+# age=19
+# t=f"my name is Ayushi,I am {age}"
+# print(t)
+
+# price=12.5567
+# txt=f"the price is {price:.2f} dollar"
+# print(txt)
+
+# output
+# my name is Ayushi,I am 19
+# the price is 12.56 dollar
+
+#Escape character-to insert characters that are illegal in a string(backlash-\)
+#for eg- using double quote inside a string surrounded by double quote
+# txt="we are the so-called \"vikings\" from the north."
+# print(txt)    #we are the so-called "vikings" from the north.
+
+#Boolean-(True or False)
+# print(10>9)           # True
+# print(10<9)           # False
+# print(bool("hello"))  # True
+# print(bool(10))       # True
+# print(bool(0))        # False
+
+# #function returning boolean
+# def fun():
+#     return True
+
+# if fun():
+#     print("yes")
+# else:
+#     print('no')
+
+#SEQUENCE DATA TYPES- LIST , TUPLE , RANGE
+ 
+#list
+l=["Ayushi",19,"21-may-2006","BTech"]
+tiny_l=["CSE",14]
+print(type(l))
+print(l)
+print(l[0])
+print(l[1:3])
+print(l[2:])
+print(tiny_l *2)
+print(l+tiny_l)
+#output
+# <class 'list'>
+# ["Ayushi",19,"21-may-2006","BTech"]
+# 
+# [19, '21-may-2006']
+# ['21-may-2006', 'BTech']
+# ['CSE', 14, 'CSE', 14]
+# ['Ayushi', 19, '16-may-2006', 'BTech', 'CSE', 14]
+
+#tuple
+tup=("abcd",789,4.56,"john",20+8j)
+tiny_tup=(123,"hello")
+print(type(tup))
+print(tup)
+print(tup[0])
+print(tup[1:3])
+print(tup[2:])
+print(tiny_tup *2)
+print(tup+tiny_tup)
+#output
+# <class 'tuple'>
+# ('abcd', 789, 4.56, 'john', (20+8j))
+# abcd
+# (789, 4.56)
+# (4.56, 'john', (20+8j))
+# (123, 'hello', 123, 'hello')
+# ('abcd', 789, 4.56, 'john', (20+8j), 123, 'hello')
+
+#range(start,stop,step)
+for i in range(5):
+    print(i)   #01234
+
+for i in range(1,10,2):
+    print(i)   #13579
